2c33f41a300be9a0488df71b1ec50fd0d34ccb78
Carlos <carlos@riv.as>
2024-05-31 10:03:44 -0700

    Added scaffolding class to implement Carter Module

--- src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductEndpoint.cs
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductEndpoint.cs
@@ -1,4 +1,6 @@
-﻿namespace Catalog.API.Products.CreateProduct
+﻿using Carter;
+
+namespace Catalog.API.Products.CreateProduct
 {
     /*
      * VERTICAL SLICE: This class represents our presentation layer
@@ -6,8 +8,16 @@
      * The folder, CreateProduct is our Vertical Slice feature
      * 
     */
-    public class CreateProductEndpoint
-    {
 
+    public record CreateProductRequest(string Name, List<string> Category, string Description, string ImageFile, decimal Price) { }
+
+    public record CreateProductResponse(Guid Id);
+
+    public class CreateProductEndpoint : ICarterModule
+    {
+        public void AddRoutes(IEndpointRouteBuilder app)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
