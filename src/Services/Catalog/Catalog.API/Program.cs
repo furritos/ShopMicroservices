77750c82e34b556a0fa8d782e6949b411c719575
Carlos <carlos@riv.as>
2024-05-31 12:24:26 -0700

    Because Carter doesn't have the ability to scan the Assembly (unlike MediatR), we resort to specifying the class file that contains the endpoint.  The other options is to move the NuGet package from the BuildingBlocks class library and onto this one.

--- src/Services/Catalog/Catalog.API/Program.cs
+++ src/Services/Catalog/Catalog.API/Program.cs
@@ -1,10 +1,13 @@
 using Carter;
-using MediatR;
+using Catalog.API.Products.CreateProduct;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add dependency injection services here
-builder.Services.AddCarter();
+builder.Services.AddCarter(null, config =>
+{
+   config.WithModule<CreateProductEndpoint>();
+});
 builder.Services.AddMediatR(config =>
 {
     config.RegisterServicesFromAssembly(typeof(Program).Assembly);
