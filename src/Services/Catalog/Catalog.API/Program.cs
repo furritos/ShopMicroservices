a5f66ce6ad66b6fcf234c8de201b8edf81251892
Carlos <carlos@riv.as>
2024-05-31 12:00:14 -0700

    Add MediatR DI

--- src/Services/Catalog/Catalog.API/Program.cs
+++ src/Services/Catalog/Catalog.API/Program.cs
@@ -1,13 +1,19 @@
 using Carter;
+using MediatR;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add dependency injection services here
 builder.Services.AddCarter();
+builder.Services.AddMediatR(config =>
+{
+    config.RegisterServicesFromAssembly(typeof(Program).Assembly);
+});
+// --------------------------------------
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline here
 app.MapCarter();
-
+// --------------------------------------
 app.Run();
