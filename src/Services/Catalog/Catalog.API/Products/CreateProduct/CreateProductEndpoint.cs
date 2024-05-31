2664ba3c91612407a0cf9c7b1b09eec37fcff809
Carlos <carlos@riv.as>
2024-05-30 21:20:00 -0700

    Document the purpose of each class file

--- src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductEndpoint.cs
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductEndpoint.cs
@@ -1,5 +1,11 @@
 ﻿namespace Catalog.API.Products.CreateProduct
 {
+    /*
+     * VERTICAL SLICE: This class represents our presentation layer
+     * 
+     * The folder, CreateProduct is our Vertical Slice feature
+     * 
+    */
     public class CreateProductEndpoint
     {
     }
--- src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -1,5 +1,11 @@
 ﻿namespace Catalog.API.Products.CreateProduct
 {
+    /*
+     * VERTICAL SLICE: This class represents our application logic layer
+     * 
+     * The folder, CreateProduct is our Vertical Slice feature
+     * 
+    */
     public class CreateProductHandler
     {
     }
