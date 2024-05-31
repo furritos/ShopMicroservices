2f51bb9b16a5d34e9d87d19dd4a2dd6aed7e2718
Carlos <carlos@riv.as>
2024-05-30 21:38:14 -0700

    Create base code for CQRS.  Use MediatR to implement Handler and Command.

--- src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -1,12 +1,30 @@
-﻿namespace Catalog.API.Products.CreateProduct
+﻿using MediatR;
+
+namespace Catalog.API.Products.CreateProduct
 {
     /*
      * VERTICAL SLICE: This class represents our application logic layer
      * 
      * The folder, CreateProduct is our Vertical Slice feature
      * 
+     * CQRS:  This handler class will be responsible for executing command logic 
+     * using MediatR library.  It will receive the "command" and process it, encapsulate
+     * business logic for specific operations.
+     * 
     */
-    public class CreateProductHandler
+
+    public record CreatedProductCommand(string Name, List<string> Category, string Description, string ImageFile, decimal Price) : IRequest<CreateProductResult>;
+
+    public record CreateProductResult(Guid Id);
+
+    internal class CreateProductCommandHandler : IRequestHandler<CreatedProductCommand, CreateProductResult>
     {
+        public Task<CreateProductResult> Handle(CreatedProductCommand request, CancellationToken cancellationToken)
+        {
+            /*
+             * Perform business logic to create a product
+             */
+            throw new NotImplementedException();
+        }
     }
 }
