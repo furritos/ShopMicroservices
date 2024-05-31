1d3a163b1d3692b029d2b250741c2d2047521324
Carlos <carlos@riv.as>
2024-05-30 21:52:29 -0700

    Create the interface that is responsible for all the commands in CQRS

---
+++ src/BuildingBlocks/BuildingBlocks/CQRS/ICommand.cs
@@ -0,0 +1,14 @@
+﻿using MediatR;
+
+namespace BuildingBlocks.CQRS
+{
+    /*
+     * <Unit> is the equivalent of void, no TResponse is expected
+     */
+    public interface ICommand: ICommand<Unit> { }
+
+    /*
+     * Returns a TReponse generic
+     */
+    public interface ICommand<out TResponse> : IRequest<TResponse> { }
+}
