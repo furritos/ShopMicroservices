d7990d8837577cef0354462b5ae3b2b14c09c94f
Carlos <carlos@riv.as>
2024-06-03 10:46:05 -0700

    Added new endpoint and  handler to get all products

---
+++ src/Services/Catalog/Catalog.API/Products/GetProducts/GetProductsEndpoint.cs
@@ -0,0 +1,33 @@
+﻿using Carter;
+using Catalog.API.Models;
+using Mapster;
+using MediatR;
+
+namespace Catalog.API.Products.GetProducts
+{
+    /*
+     * Even though we're getting all the products as a response with no input
+     * as a request, for best practice, we should still define an request 
+     * record even if it's empty as a comment.
+     */
+
+    //public record GetProductsRequest();
+    public record GetProductsResponse(IEnumerable<Product> products);
+    public class GetProductsEndpoint : ICarterModule
+    {
+        public void AddRoutes(IEndpointRouteBuilder app)
+        {
+            app.MapGet("/products", async (ISender sender) =>
+            {
+                var result = await sender.Send(new GetProductsQuery());
+                var response = result.Adapt<GetProductsResponse>();
+                return Results.Ok(response);
+            })
+            .WithName("GetProducts")
+            .Produces<GetProductsResponse>(StatusCodes.Status200OK)
+            .ProducesProblem(StatusCodes.Status400BadRequest)
+            .WithSummary("Get Products")
+            .WithDescription("This endpoint gets a list of all products");
+        }
+    }
+}
