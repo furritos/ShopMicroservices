263bb7dac4a79266bc4c5991be9273ac742c20e4
Carlos <carlos@riv.as>
2024-06-03 13:32:10 -0700

    In order for mapping to happen correctly, the GetProductByIdResult record MUST have a capitalized parameters (Product Product).  I've yet to figure out why, maybe it's something to do with how Adapt works.

--- src/Services/Catalog/Catalog.API/Products/GetProductById/GetProductByIdEndpoint.cs
+++ src/Services/Catalog/Catalog.API/Products/GetProductById/GetProductByIdEndpoint.cs
@@ -6,14 +6,14 @@ using MediatR;
 namespace Catalog.API.Products.GetProductById
 {
     //public record GetProductByIdRequest();
-    public record GetProductByIdResponse(Product product);
+    public record GetProductByIdResponse(Product Product);
     public class GetProductByIdEndpoint : ICarterModule
     {
         public void AddRoutes(IEndpointRouteBuilder app)
         {
-            app.MapGet("/products/{id}", async (Guid Id, ISender sender) =>
+            app.MapGet("/products/{id}", async (Guid id, ISender sender) =>
             {
-                var result = await sender.Send(new GetProductByIdQuery(Id));
+                var result = await sender.Send(new GetProductByIdQuery(id));
                 var response = result.Adapt<GetProductByIdResponse>();
                 return Results.Ok(response);
             }).WithName("GetProductById")
