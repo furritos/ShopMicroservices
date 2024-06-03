4b5c124668f21a9bc8b020eee91789578d587f66
Carlos Rivas <carlos@riv.as>
2024-06-02 17:12:51 -0700

    Add Marten DI

--- src/Services/Catalog/Catalog.API/Program.cs
+++ src/Services/Catalog/Catalog.API/Program.cs
@@ -1,17 +1,22 @@
 using Carter;
 using Catalog.API.Products.CreateProduct;
+using Marten;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add dependency injection services here
 builder.Services.AddCarter(null, config =>
 {
-   config.WithModule<CreateProductEndpoint>();
+    config.WithModule<CreateProductEndpoint>();
 });
 builder.Services.AddMediatR(config =>
 {
     config.RegisterServicesFromAssembly(typeof(Program).Assembly);
 });
+builder.Services.AddMarten(opts =>
+{
+    opts.Connection(builder.Configuration.GetConnectionString("Database"));
+}).UseLightweightSessions();
 // --------------------------------------
 
 var app = builder.Build();
