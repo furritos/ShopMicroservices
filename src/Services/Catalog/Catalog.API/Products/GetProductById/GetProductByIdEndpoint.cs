95130c99083e0663a2ea3c7fb9a13eb48881cee4
Carlos <carlos@riv.as>
2024-06-03 13:01:38 -0700

    CQRS: Add feature to get product by ID

---
+++ src/Services/Catalog/Catalog.API/Products/GetProductById/GetProductByIdEndpoint.cs
@@ -0,0 +1,26 @@
+﻿using Carter;
+using Catalog.API.Models;
+using Mapster;
+using MediatR;
+
+namespace Catalog.API.Products.GetProductById
+{
+    //public record GetProductByIdRequest();
+    public record GetProductByIdResponse(Product product);
+    public class GetProductByIdEndpoint : ICarterModule
+    {
+        public void AddRoutes(IEndpointRouteBuilder app)
+        {
+            app.MapGet("/products/{id}", async (Guid Id, ISender sender) =>
+            {
+                var result = await sender.Send(new GetProductByIdQuery(Id));
+                var response = result.Adapt<GetProductByIdResponse>();
+                return Results.Ok(response);
+            }).WithName("GetProductById")
+            .Produces<GetProductByIdResponse>(StatusCodes.Status200OK)
+            .ProducesProblem(StatusCodes.Status400BadRequest)
+            .WithSummary("Get product by Id")
+            .WithDescription("This endpoint gets a single product by Id");
+        }
+    }
+}
