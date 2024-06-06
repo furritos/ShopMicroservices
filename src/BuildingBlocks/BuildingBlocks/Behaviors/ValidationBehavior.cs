3ef9eaf9d527c2dd40366a03762c119b6a096fa3
Carlos <carlos@riv.as>
2024-06-06 12:17:06 -0700

    Fixed issues where, somehow, TResponse was resolved to TwoFactorAuthentication

--- src/BuildingBlocks/BuildingBlocks/Behaviors/ValidationBehavior.cs
+++ src/BuildingBlocks/BuildingBlocks/Behaviors/ValidationBehavior.cs
@@ -1,14 +1,13 @@
 using BuildingBlocks.CQRS;
 using FluentValidation;
 using MediatR;
-using Microsoft.AspNetCore.Identity.Data;
 
 namespace BuildingBlocks.Behaviors
 
 {
-    public class ValidationBehavior<TRequest, TResponse>(IEnumerable<IValidator<TRequest>> validators) : IPipelineBehavior<TRequest, TwoFactorResponse> where TRequest : ICommand<TResponse>
+    public class ValidationBehavior<TRequest, TResponse>(IEnumerable<IValidator<TRequest>> validators) : IPipelineBehavior<TRequest, TResponse> where TRequest : ICommand<TResponse>
     {
-        public async Task<TwoFactorResponse> Handle(TRequest request, RequestHandlerDelegate<TwoFactorResponse> next, CancellationToken cancellationToken)
+        public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
         {
             var context = new ValidationContext<TRequest>(request);
             var validationResults = await Task.WhenAll(validators.Select(v => v.ValidateAsync(context, cancellationToken)));
