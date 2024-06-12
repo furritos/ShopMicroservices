fbcd901c26b719f327f9808cc246602d934ebfce
Carlos <carlos@riv.as>
2024-06-12 14:40:54 -0700

    Create command query scaffold for get basket

--- src/Services/Basket/Basket.API/Basket/GetBasket/GetBasketHandler.cs
+++ src/Services/Basket/Basket.API/Basket/GetBasket/GetBasketHandler.cs
@@ -1,6 +1,15 @@
-﻿namespace Basket.API.Basket.GetBasket
+﻿using Basket.API.Models;
+using BuildingBlocks.CQRS;
+
+namespace Basket.API.Basket.GetBasket
 {
-    public class GetBasketHandler
+    public record GetBasketQuery(string Username) : IQuery<GetBasketResult>;
+    public record GetBasketResult(ShoppingCart cart);
+    internal class GetBasketQueryHandler : IQueryHandler<GetBasketQuery, GetBasketResult>
     {
+        public async Task<GetBasketResult> Handle(GetBasketQuery query, CancellationToken cancellationToken)
+        {
+            return new GetBasketResult(new ShoppingCart("carlos"));
+        }
     }
 }
