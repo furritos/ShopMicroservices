9ae78b286496fc0dc18b0d1ad9fa5d808f4e3e20
Carlos <carlos@riv.as>
2024-06-14 00:33:41 -0700

    Added new constant

--- src/BuildingBlocks/BuildingBlocks/Behaviors/ValidationBehaviorConstants.cs
+++ src/BuildingBlocks/BuildingBlocks/Behaviors/ValidationBehaviorConstants.cs
@@ -3,6 +3,7 @@
     public static class ValidationBehaviorConstants
     {
         public const string FIELD_REQUIRED = "Field is required";
+        public const string FIELD_NOT_NULL = "Field cannot be null";
         public const string FIELD_GREATER_ZERO = "Field must be greater than zero";
         public static string FieldLength(int min, int max)
         {
