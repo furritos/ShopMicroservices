2f78e8ccbfc82a1a54544b05622b2dff3a2bc592
Carlos <carlos@riv.as>
2024-06-14 11:02:23 -0700

    Wire in MediatR library with configurations and behaviors

--- src/Services/Basket/Basket.API/Program.cs
+++ src/Services/Basket/Basket.API/Program.cs
@@ -1,5 +1,9 @@
+using Basket.API.Basket.DeleteBasket;
 using Basket.API.Basket.GetBasket;
+using BuildingBlocks.Behaviors;
 using Carter;
+using FluentValidation;
+using Marten;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -11,7 +15,20 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddCarter(null, config =>
 {
     config.WithModule<GetBasketEndpoint>();
+    config.WithModule<DeleteBasketEndpoint>();
 });
+
+/*
+ * MediatR - Command/Query Abstraction Layer
+ */
+var assembly = typeof(Program).Assembly;
+builder.Services.AddMediatR(config =>
+{
+    config.RegisterServicesFromAssembly(assembly);
+    config.AddOpenBehavior(typeof(ValidationBehavior<,>));
+    config.AddOpenBehavior(typeof(LoggingBehavior<,>));
+});
+builder.Services.AddValidatorsFromAssembly(assembly);
 
 var app = builder.Build();
 
