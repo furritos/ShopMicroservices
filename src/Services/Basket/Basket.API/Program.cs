8a7598a081a921446b2ab7e7d55df27d6f9b61cf
Carlos <carlos@riv.as>
2024-06-14 09:28:06 -0700

    Wire in API endpoint for GetBasket

--- src/Services/Basket/Basket.API/Program.cs
+++ src/Services/Basket/Basket.API/Program.cs
@@ -1,6 +1,17 @@
+using Basket.API.Basket.GetBasket;
+using Carter;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to container
+
+/*
+ * Carter - API Endpoints
+ */
+builder.Services.AddCarter(null, config =>
+{
+    config.WithModule<GetBasketEndpoint>();
+});
 
 var app = builder.Build();
 
