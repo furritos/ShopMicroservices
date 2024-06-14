77ebc02f14f0fc0ac7a029c28fa963f61a4184d8
Carlos <carlos@riv.as>
2024-06-14 11:01:55 -0700

    Add DELETE Endpoint and Handler scaffolds

--- src/Services/Basket/Basket.API/Basket/DeleteBasket/DeleteBasketEndpoint.cs
+++ src/Services/Basket/Basket.API/Basket/DeleteBasket/DeleteBasketEndpoint.cs
@@ -1,6 +1,27 @@
-﻿namespace Basket.API.Basket.DeleteBasket
+﻿using Carter;
+using Mapster;
+using MediatR;
+
+namespace Basket.API.Basket.DeleteBasket
 {
-    public class DeleteBasketEndpoint
+    // public record DeleteBasketRequest(string UserName); 
+    public record DeleteBasketResponse(bool IsSuccess);
+    public class DeleteBasketEndpoint : ICarterModule
     {
+        public void AddRoutes(IEndpointRouteBuilder app)
+        {
+            app.MapDelete("/basket/{userName}", async (string userName, ISender sender) =>
+            {
+                var result = await sender.Send(userName);
+                var response = result.Adapt<DeleteBasketResponse>();
+                return Results.Ok(response);
+            })
+            .WithName("DeleteBasket")
+            .Produces<DeleteBasketResponse>(StatusCodes.Status200OK)
+            .ProducesProblem(StatusCodes.Status400BadRequest)
+            .Produces(StatusCodes.Status404NotFound)
+            .WithSummary("Delete a basket")
+            .WithDescription("This endpoint deletes a basket");
+        }
     }
 }
--- src/Services/Basket/Basket.API/Basket/DeleteBasket/DeleteBasketHandler.cs
+++ src/Services/Basket/Basket.API/Basket/DeleteBasket/DeleteBasketHandler.cs
@@ -1,6 +1,18 @@
-﻿namespace Basket.API.Basket.DeleteBasket
+﻿using Basket.API.Models;
+using BuildingBlocks.CQRS;
+using Marten;
+
+namespace Basket.API.Basket.DeleteBasket
 {
-    public class DeleteBasketHandler
+    public record DeleteBasketCommand(string UserName) : ICommand<DeleteBasketResult>;
+    public record DeleteBasketResult(bool IsSuccess);
+    internal class DeleteBasketCommandHandler(IDocumentSession session) : ICommandHandler<DeleteBasketCommand, DeleteBasketResult>
     {
+        public async Task<DeleteBasketResult> Handle(DeleteBasketCommand command, CancellationToken cancellationToken)
+        {
+            session.Delete<ShoppingCart>(command.UserName);
+            await session.SaveChangesAsync(cancellationToken);
+            return new DeleteBasketResult(true);
+        }
     }
 }
