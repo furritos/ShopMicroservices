bfe439bd7b4815db882ddf050b9dbc1e06a8153a
Carlos <carlos@riv.as>
2024-06-17 11:46:51 -0700

    Fixed two issues: 1.) Cart needs to be capitalized so Mapster can map it.  2.) The adapter needs to adapt to StoreBasketComamnd not StoreBasketRequest.

--- src/Services/Basket/Basket.API/Basket/StoreBasket/StoreBasketEndpoint.cs
+++ src/Services/Basket/Basket.API/Basket/StoreBasket/StoreBasketEndpoint.cs
@@ -5,7 +5,7 @@ using MediatR;
 
 namespace Basket.API.Basket.StoreBasket
 {
-    public record StoreBasketRequest(ShoppingCart cart);
+    public record StoreBasketRequest(ShoppingCart Cart);
     public record StoreBasketResponse(string UserName);
     public class StoreBasketEndpoint : ICarterModule
     {
@@ -13,7 +13,7 @@ namespace Basket.API.Basket.StoreBasket
         {
             app.MapPost("/basket", async (StoreBasketRequest request, ISender sender) =>
             {
-                var command = request.Adapt<StoreBasketRequest>();
+                var command = request.Adapt<StoreBasketCommand>();
                 var result = await sender.Send(command);
                 var response = result.Adapt<StoreBasketResponse>();
                 return Results.Created($"/basket/{response.UserName}", response);
