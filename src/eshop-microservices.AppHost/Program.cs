78a8cce9db53fa95517ff1607de43eb663012bfc
Carlos <carlos@riv.as>
2024-06-20 10:01:43 -0700

    Updated Dockerfile/Docker Compose orchestration, deleted unnecessary projects

--- src/eshop-microservices.AppHost/Program.cs
+++
@@ -1,7 +0,0 @@
-var builder = DistributedApplication.CreateBuilder(args);
-
-builder.AddProject<Projects.Basket_API>("basket-api");
-
-builder.AddProject<Projects.Discount_GRPC>("discount-grpc");
-
-builder.Build().Run();
