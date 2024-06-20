b869156103d48f2906561c914e16dfecd9bd86da
Carlos <carlos@riv.as>
2024-06-19 20:19:28 -0700

    Add new GRPC Microservice, Discount

--- src/eshop-microservices.AppHost/Program.cs
+++ src/eshop-microservices.AppHost/Program.cs
@@ -2,4 +2,6 @@ var builder = DistributedApplication.CreateBuilder(args);
 
 builder.AddProject<Projects.Basket_API>("basket-api");
 
+builder.AddProject<Projects.Discount_GRPC>("discount-grpc");
+
 builder.Build().Run();
