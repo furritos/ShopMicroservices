fd98962513fa30bc0c36c5e6fc02b103355a78a0
Carlos <carlos@riv.as>
2024-06-20 00:19:37 -0700

    Add initial seed data for SQLite

--- src/Services/Discount/Discount.GRPC/Data/DiscountContext.cs
+++ src/Services/Discount/Discount.GRPC/Data/DiscountContext.cs
@@ -10,5 +10,14 @@ namespace Discount.GRPC.Data
         {
             
         }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Coupon>().HasData(
+                new Coupon { Id = 1, ProductName = "IPhone X", Description = "IPhone Ten", Amount = 10 },
+                new Coupon { Id = 2, ProductName = "IPhone XXX", Description = "IPhone Sexxxy", Amount = 69 }
+                );
+            base.OnModelCreating(modelBuilder);
+        }
+
     }
 }
