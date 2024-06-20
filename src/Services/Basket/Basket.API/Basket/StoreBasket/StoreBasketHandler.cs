0ea198ed0d15bed28342c5503b02804007ea7b5b
Carlos <carlos@riv.as>
2024-06-20 15:54:53 -0700

    Added logic to deduct discount in Basket.API using GRPC service

--- src/Services/Basket/Basket.API/Basket/StoreBasket/StoreBasketHandler.cs
+++ src/Services/Basket/Basket.API/Basket/StoreBasket/StoreBasketHandler.cs
@@ -3,6 +3,7 @@ using BuildingBlocks.CQRS;
 using static BuildingBlocks.Behaviors.ValidationBehaviorConstants;
 using FluentValidation;
 using Basket.API.Data;
+using Discount.GRPC;
 
 namespace Basket.API.Basket.StoreBasket
 {
@@ -18,16 +19,25 @@ namespace Basket.API.Basket.StoreBasket
         }
     }
 
-    internal class StoreBasketCommandHandler(IBasketRepository repository) : ICommandHandler<StoreBasketCommand, StoreBasketResult>
+    public class StoreBasketCommandHandler(IBasketRepository repository, DiscountProtoService.DiscountProtoServiceClient discountProto) : ICommandHandler<StoreBasketCommand, StoreBasketResult>
     {
         public async Task<StoreBasketResult> Handle(StoreBasketCommand command, CancellationToken cancellationToken)
         {
-            ShoppingCart Cart = command.Cart;
-
-            // TODO: Store basked in database
-            // TODO: Update cache
+            await DeductDiscount(command, cancellationToken);
             await repository.StoreBasket(command.Cart, cancellationToken);
             return new StoreBasketResult(command.Cart.UserName);
+        }
+
+        private async Task DeductDiscount(StoreBasketCommand command, CancellationToken cancellationToken)
+        {
+            foreach (var item in command.Cart.Items)
+            {
+                var coupon = await discountProto.GetDiscountAsync(new GetDiscountRequest
+                {
+                    ProductName = item.ProductName
+                }, cancellationToken: cancellationToken);
+                item.Price -= coupon.Amount;
+            }
         }
     }
 }
