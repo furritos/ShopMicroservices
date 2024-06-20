54abf22c22ff7c17ccb3352bc4899ece26bc75b7
Carlos <carlos@riv.as>
2024-06-20 09:40:31 -0700

    Add CreateDiscount logic

--- src/Services/Discount/Discount.GRPC/Services/DiscountService.cs
+++ src/Services/Discount/Discount.GRPC/Services/DiscountService.cs
@@ -1,4 +1,5 @@
-﻿using Discount.GRPC.Data;
+﻿using Discount.Grpc.Models;
+using Discount.GRPC.Data;
 using Grpc.Core;
 using Mapster;
 using Microsoft.EntityFrameworkCore;
@@ -12,7 +13,7 @@ namespace Discount.GRPC.Services
         {
             var coupon = await dbContext.Coupons.FirstOrDefaultAsync(x => x.ProductName == request.ProductName);
             if (coupon is null)
-                coupon = new Grpc.Models.Coupon
+                coupon = new Coupon
                 {
                     ProductName = "No Discount",
                     Amount = 0,
@@ -20,13 +21,22 @@ namespace Discount.GRPC.Services
                 };
 
             var couponModel = coupon.Adapt<CouponModel>();
-
             return couponModel;
         }
 
-        public override Task<CouponModel> CreateDiscount(CreateDiscountRequest request, ServerCallContext context)
+        public override async Task<CouponModel> CreateDiscount(CreateDiscountRequest request, ServerCallContext context)
         {
-            return base.CreateDiscount(request, context);
+            var coupon = request.Coupon.Adapt<Coupon>();
+            if (coupon is null)
+                throw new RpcException(new Status(StatusCode.InvalidArgument, "Invalid request object."));
+
+            dbContext.Coupons.Add(coupon);
+            await dbContext.SaveChangesAsync();
+
+            logger.LogInformation("Discount is successfully created. ProductName : {ProductName}", coupon.ProductName);
+
+            var couponModel = coupon.Adapt<CouponModel>();
+            return couponModel;
         }
 
         public override Task<CouponModel> UpdateDiscount(UpdateDiscountRequest request, ServerCallContext context)
