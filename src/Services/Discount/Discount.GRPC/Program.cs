5d59cc29499659007464c94312c9a29d57de7daa
Carlos <carlos@riv.as>
2024-06-19 20:35:03 -0700

    Added new discount proto and service, enabled project to compile proto file

--- src/Services/Discount/Discount.GRPC/Program.cs
+++ src/Services/Discount/Discount.GRPC/Program.cs
@@ -1,5 +1,3 @@
-using Discount.GRPC.Services;
-
 var builder = WebApplication.CreateBuilder(args);
 
 builder.AddServiceDefaults();
@@ -12,7 +10,6 @@ var app = builder.Build();
 app.MapDefaultEndpoints();
 
 // Configure the HTTP request pipeline.
-app.MapGrpcService<GreeterService>();
 app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
 
 app.Run();
