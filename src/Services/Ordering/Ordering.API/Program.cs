057597c1317428b8eab76d0e0d9802ba7f7e8405
Carlos Rivas <carlos@riv.as>
2024-06-25 11:58:55 -0700

    Add services required by Presentation layer

--- src/Services/Ordering/Ordering.API/Program.cs
+++ src/Services/Ordering/Ordering.API/Program.cs
@@ -1,10 +1,15 @@
+using Ordering.API;
 using Ordering.Application;
+using Ordering.Infrastructure;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container (DI)
 
-builder.Services.AddApplicationServices();
+builder.Services
+    .AddApplicationServices()
+    .AddInfrastructureServices(builder.Configuration)
+    .AddAPIServices();
 
 var app = builder.Build();
 
