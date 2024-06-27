252bd102a250de5e931559ce221749e1802236f3
Carlos Rivas <carlos@riv.as>
2024-06-27 15:40:27 -0700

    Add database extension that initializes data on empty database (seed)

---
+++ src/Services/Ordering/Order.Infrastructure/Data/Extensions/InitialData.cs
@@ -0,0 +1,14 @@
+﻿using Ordering.Domain.Models;
+using Ordering.Domain.ValueObjects;
+
+namespace Ordering.Infrastructure.Data.Extensions
+{
+    internal class InitialData
+    {
+        public static IEnumerable<Customer> Customers => new List<Customer>
+        {
+            Customer.Create(CustomerId.Of(new Guid("3722996d-7c39-4bad-9c60-683f312298c9")), "john", "john@protomail.com"),
+            Customer.Create(CustomerId.Of(new Guid("33964a9a-3d0d-451f-83f6-a0ae549369df")), "kevin", "kevin@protomail.com")
+        };
+    }
+}
