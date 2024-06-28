2d299aaedfe8bdd4fbe1445db512f0fcc2e6e725
Carlos Rivas <carlos@riv.as>
2024-06-28 11:42:41 -0700

    Add domain event handler and wire in services.  With DependencyInjection.cs, because IMediator is injected in the class, we needed to add services in this manner.

--- src/Services/Ordering/Order.Infrastructure/DependencyInjection.cs
+++ src/Services/Ordering/Order.Infrastructure/DependencyInjection.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Diagnostics;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Ordering.Infrastructure.Data;
@@ -13,10 +14,13 @@ namespace Ordering.Infrastructure
         {
             var connectionString = configuration.GetConnectionString("Database");
 
-            services.AddDbContext<ApplicationDbContext>(options =>
+            services.AddScoped<ISaveChangesInterceptor, AuditableEntityInterceptor>();
+            services.AddScoped<ISaveChangesInterceptor, DispatchDomainEventsInterceptor>();
+
+            services.AddDbContext<ApplicationDbContext>((sp, options) =>
             {
                 options.UseSqlServer(connectionString);
-                options.AddInterceptors(new AuditableEntityInterceptor());
+                options.AddInterceptors(sp.GetServices<ISaveChangesInterceptor>());
             });
 
             //services.AddScoped<IApplicationDbContext, ApplicationDbContext>();
