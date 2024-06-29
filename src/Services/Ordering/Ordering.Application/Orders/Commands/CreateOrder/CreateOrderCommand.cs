32317334af539561920f37ce7ed30f9c0776e565
Carlos Rivas <carlos@riv.as>
2024-06-29 12:45:13 -0700

    Add CQRS classes

---
+++ src/Services/Ordering/Ordering.Application/Orders/Commands/CreateOrder/CreateOrderCommand.cs
@@ -0,0 +1,18 @@
+﻿using BuildingBlocks.CQRS;
+using FluentValidation;
+using Ordering.Application.DTO;
+
+namespace Ordering.Application.Orders.Commands.CreateOrder
+{
+    public record CreateOrderCommand(OrderDTO Order) : ICommand<CreateOrderResult>;
+    public record CreateOrderResult(Guid OId);
+    public class CreateOrderCommandValidator : AbstractValidator<CreateOrderCommand>
+    {
+        public CreateOrderCommandValidator()
+        {
+            RuleFor(x => x.Order.OrderName).NotEmpty().WithMessage("Name is required");
+            RuleFor(x => x.Order.CustomerId).NotNull().WithMessage("CustomerId is required");
+            RuleFor(x => x.Order.OrderItems).NotEmpty().WithMessage("OrderItems should not be empty");
+        }
+    }
+}
---
+++ src/Services/Ordering/Ordering.Application/Orders/Commands/CreateOrder/CreateOrderHandler.cs
@@ -0,0 +1,45 @@
+﻿using BuildingBlocks.CQRS;
+using Ordering.Application.Data;
+using Ordering.Application.DTO;
+using Ordering.Domain.Models;
+using Ordering.Domain.ValueObjects;
+
+namespace Ordering.Application.Orders.Commands.CreateOrder;
+public class CreateOrderHandler(IApplicationDbContext dbContext)
+    : ICommandHandler<CreateOrderCommand, CreateOrderResult>
+{
+    public async Task<CreateOrderResult> Handle(CreateOrderCommand command, CancellationToken cancellationToken)
+    {
+        //create Order entity from command object
+        //save to database
+        //return result 
+
+        var order = CreateNewOrder(command.Order);
+
+        dbContext.Orders.Add(order);
+        await dbContext.SaveChangesAsync(cancellationToken);
+
+        return new CreateOrderResult(order.Id.Value);
+    }
+
+    private Order CreateNewOrder(OrderDTO orderDTO)
+    {
+        var shippingAddress = Address.Of(orderDTO.ShippingAddress.FirstName, orderDTO.ShippingAddress.LastName, orderDTO.ShippingAddress.EmailAddress, orderDTO.ShippingAddress.AddressLine, orderDTO.ShippingAddress.Country, orderDTO.ShippingAddress.State, orderDTO.ShippingAddress.ZipCode);
+        var billingAddress = Address.Of(orderDTO.BillingAddress.FirstName, orderDTO.BillingAddress.LastName, orderDTO.BillingAddress.EmailAddress, orderDTO.BillingAddress.AddressLine, orderDTO.BillingAddress.Country, orderDTO.BillingAddress.State, orderDTO.BillingAddress.ZipCode);
+
+        var newOrder = Order.Create(
+                id: OrderId.Of(Guid.NewGuid()),
+                customerId: CustomerId.Of(orderDTO.CustomerId),
+                orderName: OrderName.Of(orderDTO.OrderName),
+                shippingAddress: shippingAddress,
+                billingAddress: billingAddress,
+                payment: Payment.Of(orderDTO.Payment.CardName, orderDTO.Payment.CardNumber, orderDTO.Payment.Expiration, orderDTO.Payment.Cvv, orderDTO.Payment.PaymentMethod)
+                );
+
+        foreach (var orderItemDTO in orderDTO.OrderItems)
+        {
+            newOrder.Add(ProductId.Of(orderItemDTO.ProductId), orderItemDTO.Quantity, orderItemDTO.Price);
+        }
+        return newOrder;
+    }
+}
