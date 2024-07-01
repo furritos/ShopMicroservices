dc4c94158827b4c37a6217769aa847f6f8e4404d
Carlos Rivas <carlos@riv.as>
2024-07-01 14:40:10 -0700

    Add Get Order by Customer Handler/Query

---
+++ src/Services/Ordering/Ordering.Application/Orders/Queries/GetOrdersByCustomer/GetOrdersByCustomerQuery.cs
@@ -0,0 +1,8 @@
+﻿using BuildingBlocks.CQRS;
+using Ordering.Application.DTO;
+
+namespace Ordering.Application.Orders.Queries.GetOrdersByCustomer
+{
+    public record GetOrdersByCustomerQuery(Guid CustomerId) : IQuery<GetOrdersByCustomerResult>;
+    public record GetOrdersByCustomerResult(IEnumerable<OrderDTO> orders);
+}
