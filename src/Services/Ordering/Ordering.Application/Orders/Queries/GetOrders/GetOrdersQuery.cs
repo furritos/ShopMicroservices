23b4a8c265ba872cae70f2a5866076c0db6bccbc
Carlos Rivas <carlos@riv.as>
2024-07-01 15:13:20 -0700

    Add new query, Get Orders

---
+++ src/Services/Ordering/Ordering.Application/Orders/Queries/GetOrders/GetOrdersQuery.cs
@@ -0,0 +1,9 @@
+﻿using BuildingBlocks.CQRS;
+using BuildingBlocks.Pagination;
+using Ordering.Application.DTO;
+
+namespace Ordering.Application.Orders.Queries.GetOrders
+{
+    public record GetOrdersQuery(PaginationRequest PaginationRequest) : IQuery<GetOrdersResult>;
+    public record GetOrdersResult(PaginationResult<OrderDTO> Orders);
+}
